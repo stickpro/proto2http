3312b13b1a2524c6760992e402f6a3caf91d7d99
Vladislav Bulagakov <stick.qwe@gmail.com>
2026-01-21 09:54:55 +0300

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,21 +29,21 @@ kotlin {
 intellijPlatform {
     pluginVerification {
         ides {
-            ide("IC", "2024.1")  // IntelliJ IDEA Community
-            ide("IU", "2024.1")  // IntelliJ IDEA Ultimate
-            ide("PS", "2024.1")  // PhpStorm
-            ide("WS", "2024.1")  // WebStorm
-            ide("PY", "2024.1")  // PyCharm
-            ide("GO", "2024.1")  // GoLand
-            ide("CL", "2024.1")  // CLion
-            ide("RD", "2024.1")  // Rider
+            ide("IC", "2024.3.7")  // IntelliJ IDEA Community
+            ide("IU", "2024.3.7")  // IntelliJ IDEA Ultimate
+            ide("PS", "2024.3.6")  // PhpStorm
+            ide("WS", "2024.3.5")  // WebStorm
+            ide("PY", "2024.3.5")  // PyCharm
+            ide("GO", "2024.3.5")  // GoLand
+            ide("CL", "2024.3.5")  // CLion
+            ide("RD", "2024.3.6")  // Rider
         }
     }
 }
 
 tasks {
     patchPluginXml {
-        sinceBuild.set("241")
+        sinceBuild.set("243")
         untilBuild.set("253.*")
     }
 
